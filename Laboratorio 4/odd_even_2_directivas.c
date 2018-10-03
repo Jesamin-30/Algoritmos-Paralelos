d52836e68c12d9fc7db3fad654430bfffe19737e
Jesamin-30 <31942577+Jesamin-30@users.noreply.github.com>
2018-10-02 19:12:14 -0500

    Add files via upload

---
+++ Laboratorio 4/odd_even_2_directivas.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int thread_count;
+void odd_even_openmp(int *vector, int n){
+    int phase, i, tmp;
+    #  pragma omp parallel num_threads(thread_count) default(none) shared(a, n) private(i, tmp, phase)
+    for(phase = 0; phase < n; phase++){
+        if(phase % 2 == 0){
+            #pragma omp for 
+                for(i=1; i<n; i+=2){
+                    if(vector[i-1] > vector[i]){
+                       tmp = vector[i-1];
+                       vector[i-1] = vector[i];
+                       vector[i] = tmp;
+                    }
+                }
+        }
+        else{
+            #pragma omp for 
+                for(i=1; i<n-1; i+=2){
+                    if (vector[i] > vector[i+1]){
+                       tmp = vector[i+1];
+                       vector[i+1] = vector[i];
+                       vector[i] = tmp;
+                    }
+                }
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    int  n;
+    int* vector;
+    double inicio, final;
+    int thread_count= strtol(argv[1], NULL, 10); //Get number of threads from command line 
+
+    n = 20000;
+    vector = malloc(n*sizeof(int));
+   
+    int i;
+    srand(time(NULL));
+    for(i=0; i<n; i++){
+        vector[i] = rand()%n;
+    }
+
+    inicio = omp_get_wtime();
+    odd_even_openmp(vector,n);
+    final = omp_get_wtime();
+
+    printf("tiempo = %.6lf \n", final-inicio);
+
+    free(vector);
+    return 0;
+}
